05872bfd299fa5fb79a2451b5cb8bc41f8d563f0
Vithushan <sutharshanvithushan@gmail.com>
2025-06-14 16:41:01 +0530

    grid

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {books &&
             (Object.values(books) as Book[]).map((book) => (
               <BookCard

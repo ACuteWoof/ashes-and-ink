e81507430eda03c3b5db90218db3646f81135cd8
Vithushan <sutharshanvithushan@gmail.com>
2025-06-15 09:29:32 +0530

    metadata: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "Ashes & Ink",
   description:
     "At Ashes & Ink, we aim for the best quality we could get with the design of each book, and for the benefit of you, fellow rebel, we also print books that you may request, in case we missed out on a valuable piece of writing in our store.",
+  icons: "/icon.png"
 };
 
 export default function RootLayout({

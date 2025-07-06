31f937944c15de39880c34008d5259d1682f5628
Vithushan <sutharshanvithushan@gmail.com>
2025-07-07 04:59:40 +0530

    remove supabase

--- app/supabase.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://paybtogwieqntyllfxry.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)

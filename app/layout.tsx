662c5d9f257bc0c9b295991d9358e9729e64646c
Vithushan <sutharshanvithushan@gmail.com>
2025-12-16 11:33:14 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { base } from "./fonts";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Ashes & Ink",
@@ -18,6 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${base.className} antialiased scroll-smooth`}>
         {children}
+	<Analytics />
+	<SpeedInsights />
       </body>
     </html>
   );

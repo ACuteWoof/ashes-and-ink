8325925f117268fdbaf48b5f9854c232ad427db0
Vithushan <sutharshanvithushan@gmail.com>
2026-02-18 23:35:18 +0530

    better book grid

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
 	      className="max-w-screen-sm"
             />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-8">
           {books &&
             (
               Object.values(books).toSorted((a, b) =>

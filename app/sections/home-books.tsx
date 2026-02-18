1ba3d2b8cb1eca98fad16cbc115ab580e037696b
Vithushan <sutharshanvithushan@gmail.com>
2026-02-18 23:31:15 +0530

    book lists

--- app/sections/home-books.tsx
+++ app/sections/home-books.tsx
@@ -12,7 +12,7 @@ export default function HomeBooks() {
           <h1 className={"text-3xl " + caligraphy.className}>Our Books</h1>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-8 items-center">
         {books &&
           (
             Object.values(books).toSorted((a, b) =>
